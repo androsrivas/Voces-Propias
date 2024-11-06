6af558d8208a67d23fc821d0c2d05345eb0892f8
dustisme <androsrivas@outlook.com>
2024-11-06 18:28:08 +0100

    start header component. to finish later

---
+++ src/components/molecules/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from "../../atoms/Breadcrumb/Breadcrumb";
+
+function Header() {
+  return (
+    <>
+        {/* icon */}
+        <Breadcrumb />
+    </>
+  )
+}
+
+export default Header
