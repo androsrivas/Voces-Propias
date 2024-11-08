0dc2dfacdbd0028fef3b6fce243eeda41df347c4
dustisme <androsrivas@outlook.com>
2024-11-08 11:46:37 +0100

    delete test router path

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,17 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import GetBooks from "../pages/Admin/GetBooks/GetBooks";
 import Layout from "../layout/Admin/Layout";
 import AddItemForm from "../components/molecules/Form/AddItemForm/AddItemForm";
-import Test from "../pages/Admin/test";
-// A medida que tengamos las pages hechas, se añaden en element en su correspondiente path
 
 export const router = createBrowserRouter([
-    // path for testing
+    
     {
         path: "/",
-        element: <Test />
-    },
-    {
-        path: "/app",
         element:<Layout />,
         children: [
             {
