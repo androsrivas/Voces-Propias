2528186396a8a5b2392072123d7045514ead72da
dustisme <androsrivas@outlook.com>
2024-11-08 11:49:47 +0100

    delete unnecesary path on router

---
+++ src/components/molecules/MenuMobile/MenuMobile.jsx
@@ -0,0 +1,11 @@
+
+
+const MenuMobile = () => {
+  return (
+    <>
+        <p>Este es mi menu version mobile</p>
+    </>
+  )
+}
+
+export default MenuMobile
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,43 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-import CreateBook from "../pages/CreateBook/CreateBook";
+// import CreateBook from "../pages/CreateBook/CreateBook";
 import Books from "../pages/Books/Books";
 import Layout from "../layout/Layout";
-// A medida que tengamos las pages hechas, se añaden en element en su correspondiente path
+
 
 export const router = createBrowserRouter([
-    // Primero el path para cliente admin
+
     {
-        path: "/app",
+        path: "/",
         element:<Layout />,
         children: [
             {
-                path: "/app/books",
+                path: "books",
                 element: <Books />
             },
             {
-                path: "/app/new-book",
-                element: <CreateBook />
-            }
-        ]
-    },
-    // Segundo el path para usuario
-    {
-        path: "/",
-        element: "",
-        children: [
-            {
-                path: "/home",
+                path: "new-book",
                 element: ""
-            },
-            {
-                path: "/books",
-                element: "",
-                children: [
-                    {
-                        path: "/books:id",
-                        element: ""
-                    }
-                ]
             }
         ]
     }
