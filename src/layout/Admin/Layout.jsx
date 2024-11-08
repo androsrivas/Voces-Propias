b5521e9408a34fe6f036ac7d32914497e7b8d4ee
dustisme <androsrivas@outlook.com>
2024-11-08 13:00:42 +0100

    apply custom hook to layout

--- src/layout/Admin/Layout.jsx
+++ src/layout/Admin/Layout.jsx
@@ -2,13 +2,23 @@ import { Outlet } from "react-router-dom";
 import useScreenSize from "../../hooks/useScreenSize/useScreenSize";
 import MenuMobile from "../../components/molecules/MenuMobile/MenuMobile";
 import Menu from "../../components/molecules/Menu/Menu";
+import { FaHome } from "react-icons/fa";
+
+
 
 const Layout = () => {
   const width = useScreenSize();
 
   return (
     <>
-      {width < 768 ? <MenuMobile /> : <Menu />}
+      {
+        width < 768 ? 
+        <MenuMobile 
+          className="menuMobile"
+          icon={ FaHome }
+        /> : 
+        <Menu className="menu"/>
+      }
       <Outlet />
         
     </>
