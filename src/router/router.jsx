b735f5f95056e1d448513c0599f8758c2d66bc44
dustisme <androsrivas@outlook.com>
2024-11-11 09:29:58 +0100

    add routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Admin/Layout";
+import GetBooks from "../pages/Admin/GetBooks/GetBooks";
+import CreateBook from "../pages/Admin/CreateBook/CreateBook";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />
+        element: <Layout />,
+        children: [
+            {
+                path: "books",
+                element: <GetBooks />
+            },
+            {
+                path: "new-book",
+                element: <CreateBook />
+            }
+        ]
     }
 ]);
