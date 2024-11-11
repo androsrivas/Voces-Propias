0f519b4c6fd7e2b76d8362a4ad353d9e8a52c6d5
Cristina Romero <criromllo@gmail.com>
2024-11-11 14:13:35 +0100

    cambiar rutas

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,11 +3,12 @@ import Layout from "../layout/Admin/Layout";
 import GetBooks from "../pages/Admin/GetBooks/GetBooks";
 import CreateBook from "../pages/Admin/CreateBook/CreateBook";
 import LayoutUser from "../layout/User/LayoutUser";
+import ProductListView from "../pages/User/ProductListView/ProductListView";
 
 
 export const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/dashboard",
         element: <Layout />,
         children: [
             {
